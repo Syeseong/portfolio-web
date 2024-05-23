6cf3fcee59225620f4dab45ad0541b5474e26069
Syeseong <sy051398@naver.com>
2024-05-23 23:04:52 +0900

    feat : 24-05-23 - 기초 세팅

---
+++ src/pages/Home.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Skip from '../components/Skip'
+import Header from '../components/Header'
+import Intro from '../components/Intro'
+import Stack from '../components/Stack'
+import Port from '../components/Port'
+import Contact from '../components/Contact'
+import Footer from '../components/Footer'
+import Sns from '../components/Sns'
+import Main from '../components/Main'
+
+const Home = () => {
+    return (
+        <>
+            <Skip />
+            <Header />
+            <Sns />
+            <Main>
+                <Intro />
+                <Stack />
+                <Port />
+                <Contact />
+            </Main>
+            <Footer />
+        </>
+    )
+}
+
+export default Home
