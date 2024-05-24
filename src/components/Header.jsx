d580e4e370b097e558826891da23f3cd8a5ba24d
Syeseong <sy051398@naver.com>
2024-05-25 04:14:38 +0900

    feat : 24-05-25 - Header 영역(데스크탑)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-
+import React from "react"
+import navItem from "../data/navItems.json"
 const Header = () => {
     return (
-        <div>
-            Header
-        </div>
+        <header id="header" role="banner">
+            <div className="header__inner">
+                <div className="header__logo">
+                    <h1>
+                        <a href="/"><span>PORTFOLIO</span><em><span>2024</span> 서예성</em></a>
+                    </h1>
+                </div>
+                <nav className="header__nav">
+                    <ul>
+                        {navItem.map((item, idx) => (
+                            <li key={idx}>
+                                <a href={item.link}><p>0{+ idx + 1}.</p>{item.name}</a>
+                            </li>
+                        ))}
+                    </ul>
+                </nav>
+            </div>
+        </header>
     )
 }
 
--- src/components/Skip.jsx
+++ src/components/Skip.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 const Skip = () => {
     return (
-        <div>
-            Skip
+        <div id="skip">
+            <a href="#intro">인트로 영역 바로가기</a>
+            <a href="#stack">스택 영역 바로가기 </a>
+            <a href="#port">포트폴리오 영역 바로가기</a>
+            <a href="#contact">연락처 영역 바로가기</a>
         </div>
     )
 }
