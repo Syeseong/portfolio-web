6ce3bdee90de7b53fc3f0dee95acc72b2f93d6b1
Syeseong <sy051398@naver.com>
2024-06-07 01:14:13 +0900

    feat : 24-06-07 - Port 영역 (데스크탑)

--- src/components/Port.jsx
+++ src/components/Port.jsx
@@ -1,11 +1,47 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger); //GSAP ScrollTrigger 플러그인 등록
 
 const Port = () => {
+    const portRef = useRef(null); //특정 DOM 요소를 참조하기 위해 사용
+
+    useEffect(() => {
+        const panels = gsap.utils.toArray(".port__item"); //해당 클래스가 적용된 모든 요소를 배열로 가져옴
+        const container = portRef.current; //위에서 정의한 portRef가 참조하는 현재 DOM요소를 가져옴
+
+        gsap.to(panels, {
+            xPercent: -100 * (panels.length - 1), //X축 이동하는 비율 현재설정 -100%
+            ease: "none", //애니메이션 속도 설정
+            scrollTrigger: {
+                trigger: container, //애니메이션이 시작될 DOM 요소 지정
+                pin: true, //스크롤 동안 요소 고정
+                scrub: 1, // 스크롤과 애니메이션의 싱크를 맞춤
+                pinSpacing: true,
+                end: () => `+=${container.offsetWidth * (panels.length - 1)}` //애니메이션이 끝나는 시점을 설정,
+            }
+        });
+    }, []);
+
     return (
-        <div>
-            Port
+        <div id="port" ref={portRef}>
+            <div className="port__inner">
+                <div className="port__wrap">
+                    <div className="port__item p1">
+                        <ul>
+                            <li>my front</li>
+                            <li>portfolio</li>
+                        </ul>
+                    </div>
+                    <div className="port__item p2">page 1</div>
+                    <div className="port__item p3">page 2</div>
+                    <div className="port__item p4">page 3</div>
+                    <div className="port__item p5">page 4</div>
+                </div>
+            </div>
         </div>
-    )
+    );
 }
 
-export default Port
+export default Port;
